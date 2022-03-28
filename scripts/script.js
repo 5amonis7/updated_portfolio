6854a2c63c1b3472ebd93687c54b76b00769753b
Andrew Monis <5andrewmonis7@gmail.com>
2022-03-28 13:27:12 -0400

    fixing animation for mobile

--- scripts/script.js
+++ scripts/script.js
@@ -51,6 +51,36 @@
 
       });
 
+// Responsive animation
+
+let width = screen.width;
+headerInfo()
+
+function headerInfo(){
+  if(width > 700){
+    document.addEventListener('scroll' , function(){
+          if(isInViewPort(skills) === true){
+            ball1.style.animation = 'roll-left 2s none';
+            ball2.style.animation = 'roll-up 2s none';
+            ball3.style.animation = 'roll-right 2s none';
+            set.forEach((e) => {
+              e.style.visibility = 'visible';
+            }) 
+          }
+        })
+    }
+    else{
+      if(isInViewPort(skills) === true){
+        ball1.style.animation = 'roll-left 2s none';
+        ball3.style.animation = 'roll-up 2s none';
+        ball2.style.animation = 'roll-right 2s none';
+        set.forEach((e) => {
+          e.style.visibility = 'visible';
+        }) 
+    }
+  }
+}
+
 
 // Whether or not Top Btn shows
 function isInViewPort(e){
@@ -96,17 +126,6 @@ window.onload = function(){
   })
 } 
 
-document.addEventListener('scroll' , function(){
-  if(isInViewPort(skills) === true){
-    ball1.style.animation = 'roll-left 2s none';
-    ball2.style.animation = 'roll-up 2s none';
-    ball3.style.animation = 'roll-right 2s none';
-    set.forEach((e) => {
-      e.style.visibility = 'visible';
-    }) 
-  }
-})
-
 
 document.querySelectorAll('.letter').forEach(el=>{
   el.addEventListener('mouseover', () => {
@@ -150,120 +169,6 @@ function sliderScrollRight(){
 }
 
 
-
-
-
-// const slide = document.querySelector(".projects-box");
-// let slides = document.querySelectorAll(".project");
-// const leftArrow = document.querySelector('#left-arrow');
-// const rightArrow = document.querySelector('#right-arrow');
-// let index = 1;
-
-// const slideWidth = slides[index].clientWidth + 64;
-
-
-// rightArrow.addEventListener('click', () => {
-//   if(index >= slides.length - 1) return
-//   index++;
-//   slide.style.transform = `translateX(${-slideWidth * index}px)`
-//   slide.style.transition = '.7s';
-// })
-
-// leftArrow.addEventListener('click', () => {
-//   if(index <= 0) return
-//   index--;
-//   slide.style.transform = `translateX(${-slideWidth * index}px)`
-//   slide.style.transition = '.7s';
-//   leftArrow.disabled = true;
-// })
-
-
-
-
-
-
-
-// let index = 1;
-// let slideId;
-// const interval = 500;
-
-// const firstClone = slides[0].cloneNode(true);
-// const secondClone = slides[1].cloneNode(true);
-// const thirdClone = slides[2].cloneNode(true);
-// const fourthClone = slides[3].cloneNode(true);
-// const lastClone = slides[5].cloneNode(true);
-
-
-// firstClone.id = 'first-clone';
-// lastClone.id = 'last-clone';
-
-
-// slide.append(firstClone);
-// slide.append(secondClone);
-// slide.append(thirdClone);
-// slide.append(fourthClone);
-// slide.prepend(lastClone);
-
-
-// const slideWidth = slides[index].clientWidth + 64;
-
-// Autoplay turned off
-// const startSlide = () => {
-//   slideId = setInterval(() => {
-//     index++;
-//     slide.style.transform = `translateX(${-slideWidth * index}px)`
-//     slide.style.transition = '.7s';
-//   }, interval)
-// }
-
-// slide.addEventListener('mouseenter', () => {
-//   clearInterval(slideId);
-// })
-// slide.addEventListener('mouseleave', startSlide)
-
-// startSlide()
-
-// slide.addEventListener('transitionend', () => {
-//   slides = document.querySelectorAll(".project");
-//   if(slides[index].id === firstClone.id){
-//     slide.style.transition = 'none';
-//     index = 1;
-//     slide.style.transform = `translateX(${-slideWidth * index}px)`
-//   }
-//   else if(slides[index].id === lastClone.id){
-//     slide.style.transition = 'none';
-//     index = slides.length - 5;
-//     slide.style.transform = `translateX(${-slideWidth * index}px)`
-//   }
-// })
-
-// function moveToNextSlide(){
-//   slides = document.querySelectorAll(".project");
-//   if(index >= slides.length - 1) return
-//   index++;
-//   slide.style.transform = `translateX(${-slideWidth * index}px)`
-//   slide.style.transition = '.7s';
-//   rightArrow.disabled = true;
-//   setTimeout(function () {
-//     rightArrow.disabled = false;
-//   }, 800)
-// }
-
-// function moveToPreviousSlide(){
-//   if(index <= 0) return
-//   index--;
-//   slide.style.transform = `translateX(${-slideWidth * index}px)`
-//   slide.style.transition = '.7s';
-//   leftArrow.disabled = true;
-//   setTimeout(function () {
-//     leftArrow.disabled = false;
-//   }, 800)
-// }
-
-
-// rightArrow.addEventListener('click', moveToNextSlide)
-// leftArrow.addEventListener('click', moveToPreviousSlide)
-
 // Mobile Nav
 const hamburger = document.querySelector('.hamburger');
 const hamburgerIcon = document.querySelector('.hamburger-icon');
@@ -297,16 +202,4 @@ mobileItem.forEach((e) => {
   })
 })
 
-// Responsive
-// let width = screen.width;
-
-// function headerInfo(){
-//   if(width === 910){
-//     card.forEach((e) => {
-//       e.addEventListener('click', () => {
-
-//       })
-//     })
-//   }
-// }
 
